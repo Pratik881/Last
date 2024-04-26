f2dd1a98b655f3b0272d69b34cf577c576aa0317
Pratik <pratikn484@gmail.com>
2024-04-26 10:21:39 +0545

    ignored

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const cors=require('cors')
+require('dotenv').config()
 const myConnection=require('../Backend/src/Config/dbConfig')
 const routes=require('../Backend/src/Routes/blogRoutes')
 const app=express()
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:process.env.frontend_url,
     methods:['GET','POST','PUT','DELETE'],
     credentials:true
 }))
--- Backend/src/Config/dbConfig.js
+++ Backend/src/Config/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const myConnection=async()=>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/project')
+        await mongoose.connect(process.env.url)
         console.log('Connection successfull')
     }
     catch(err){
